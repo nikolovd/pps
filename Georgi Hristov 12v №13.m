6e2616dda64c8d39a596fc6ab333309126b72c61
Georgi Hristov <g.h.hristov9@gmail.com>
2017-05-07 22:59:07 +0300

    Add files via upload

---
+++ Georgi Hristov 12v №13.m
@@ -0,0 +1,17 @@
+rng(13)%
+A=(1000+1000).*rand(1000,1000) - 1000;
+A1=sum(A);
+A2=round(A);
+A3=sum(A2);
+B=[(15-13)*rand(1)*10, (15-13)*rand(1)*60]
+disp(sum(A3(1)))
+C=A3(120:130)
+E=det(A)
+X=eye(1000)
+F=A*X
+x=0:0.001:3*pi/4
+y1=cos(sqrt(x))
+y2 = sin(x.*x)
+G=plot(x,y1,x,y2)
+D = max(C,[],2)
+put labels and panels
